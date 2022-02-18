0c8c0b64e01d2ffdfb54016984a178f930f81b3b
David Andres Leon <david.andres.leon@iaac.net>
2022-02-18 17:09:47 +0100

    string test

--- strings/script.js
+++ strings/script.js
@@ -5,11 +5,28 @@ import rhino3dm from "https://cdn.jsdelivr.net/npm/rhino3dm@7.11.1/rhino3dm.modu
 import { RhinoCompute } from "https://cdn.jsdelivr.net/npm/compute-rhino3d@0.13.0-beta/compute.rhino3d.module.js";
 import { Rhino3dmLoader } from "https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/loaders/3DMLoader.js";
 
-const definitionName = "string.gh";
+const definitionName = "strings.gh";
 
-const count_slider = document.getElementById("count");
-count_slider.addEventListener("mouseup", onSliderChange, false);
-count_slider.addEventListener("touchend", onSliderChange, false);
+const input = document.querySelector('input');
+const submitButton = document.querySelector('button');
+let  log = document.getElementById('values');
+submitButton.addEventListener('click', submitText);
+
+let input_string;
+
+//console.log(input_string)
+
+
+function submitText(){
+    input_string = input.value
+    log.innerHTML = input_string
+    input.value = "";
+    compute();
+};
+
+
+
+
 
 const loader = new Rhino3dmLoader();
 loader.setLibraryPath("https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/");
@@ -19,9 +36,7 @@ rhino3dm().then(async (m) => {
   console.log("Loaded rhino3dm.");
   rhino = m; // global
 
-  RhinoCompute.url = getAuth( 'RHINO_COMPUTE_URL' ) // RhinoCompute server url. Use http://localhost:8081 if debugging locally.
-  RhinoCompute.apiKey = getAuth( 'RHINO_COMPUTE_KEY' )  // RhinoCompute server api key. Leave blank if debugging locally.
-    
+ RhinoCompute.url = "http://localhost:8081/"; //debugging locally.
 
 
   const url = definitionName;
@@ -30,21 +45,14 @@ rhino3dm().then(async (m) => {
   const arr = new Uint8Array(buffer);
   definition = arr;
 
-  init();
-  compute();
 });
 
 
 async function compute() {
 
-  const input = document.querySelector('input');
-  const log = document.getElementById('values');
-  
-  input.addEventListener('input', updateValue);
-
-  function updateValue(e) {
-    log.textContent = e.target.value;
-  }
+  const param1 = new RhinoCompute.Grasshopper.DataTree("Path");
+  console.log(typeof input_string)
+  param1.append([0], [input_string]);
 
   // clear values
   const trees = [];
@@ -55,148 +63,19 @@ async function compute() {
     definition,
     trees
   );
-
-
-  doc = new rhino.File3dm();
-
-  document.getElementById("loader").style.display = "none";
+  
+  console.log(res)
 
   for (let i = 0; i < res.values.length; i++) {
     for (const [key, value] of Object.entries(res.values[i].InnerTree)) {
       for (const d of value) {
         const data = JSON.parse(d.data);
-        const rhinoObject = rhino.CommonObject.decode(data);
-
-        doc.objects().add(rhinoObject, null);      }
+        console.log(data)
+        //const rhinoObject = rhino.CommonObject.decode(data);
+        //doc.objects().add(rhinoObject, null);      }
     }
   }
 
-
-
-  //transfer geometry userstring to object attribute for some objects
-  let objects = doc.objects();
-  for ( let i = 0; i < objects.count; i++ ) {
-    const rhinoObject = objects.get( i );
-
-    if ( rhinoObject.geometry().userStringCount > 0 ) {
-      const g_userStrings = rhinoObject.geometry().getUserStrings()
-      if( g_userStrings[0][0] == "geo") {
-        rhinoObject.attributes().setUserString(g_userStrings[0][0], g_userStrings[0][1])
-      }
-    }
-  }
-
-
-  scene.traverse((child) => {
-    if (!child.isLight) {
-      scene.remove(child);
-    }
-  });
-
-  const buffer = new Uint8Array(doc.toByteArray()).buffer;
-  loader.parse(buffer, function (object) {
-    
-
-    // go through all objects, check for userstrings and assing colors
-
-    object.traverse((child) => {
-
-      if(child.isLine){
-        console.log(child)
-
-        if (child.hasOwnProperty("userStringCount") && child.userData.attributes.userStringCount > 0) {
-          
-            //get color from userStrings
-            const colorData = child.userData.attributes.userStrings[0]
-            const col = colorData[1];
-
-            //convert color from userstring to THREE color and assign it
-            const threeColor = new THREE.Color("rgb(" + col + ")");
-            const mat = new THREE.LineBasicMaterial({ color: threeColor });
-            child.material = mat;
-          
-        }
-
-
-    }
-
-    });
-
-    ///////////////////////////////////////////////////////////////////////
-    // add object graph from rhino model to three.js scene
-    scene.add(object);
-
-  });
 }
 
-function onSliderChange() {
-  // show spinner
-  document.getElementById("loader").style.display = "block";
-  compute();
-}
-
-
-// THREE BOILERPLATE //
-let scene, camera, renderer, controls;
-
-function init() {
-  // create a scene and a camera
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(1, 1, 1);
-  camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  camera.position.z = -30;
-
-  // create the renderer and add it to the html
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-
-  // add some controls to orbit the camera
-  controls = new OrbitControls(camera, renderer.domElement);
-
-  // add a directional light
-  const directionalLight = new THREE.DirectionalLight(0xffffff);
-  directionalLight.intensity = 2;
-  scene.add(directionalLight);
-
-  const ambientLight = new THREE.AmbientLight();
-  scene.add(ambientLight);
-
-  animate();
-}
-
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-}
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  animate();
-}
-
-function meshToThreejs(mesh, material) {
-  const loader = new THREE.BufferGeometryLoader();
-  const geometry = loader.parse(mesh.toThreejsJSON());
-  return new THREE.Mesh(geometry, material);
-}
-
-function getAuth( key ) {
-  let value = localStorage[key]
-  if ( value === undefined ) {
-      const prompt = key.includes('URL') ? 'Server URL' : 'Server API Key'
-      value = window.prompt('RhinoCompute ' + prompt)
-      if ( value !== null ) {
-          localStorage.setItem( key, value )
-      }
-  }
-  return value
-}
 
